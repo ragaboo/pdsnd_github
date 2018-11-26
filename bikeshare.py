58ee0ffea58d53839032cab898a94e3e44c5a4d2
Shawn Green <green.shawn.p@gmail.com>
2018-11-25 19:52:11 -0800

    docs: Add clarifications to bikeshare.py comments

--- bikeshare.py
+++ bikeshare.py
@@ -129,7 +129,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users, including user type, gender, and birth year"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
